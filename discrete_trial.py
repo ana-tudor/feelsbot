580a6ebb696e18403d48a77aa9a3e2de432c3a34
Ana Tudor <anamtudor@gmail.com>
2018-02-17 11:04:37 -0800

    Added eclipse project for work with Google Cloud API

---
+++ discrete_trial.py
@@ -0,0 +1,38 @@
+class DiscreteTrial(object):
+    def __init__(categ, plan, num):
+        self.categ = categ
+        self.plan = plan
+        self.num = num
+
+    '''
+    Runs setup of trial
+    '''
+    def run_antecedent():
+        return
+
+    '''
+    Runs bot's prompt for trial
+    '''
+    def run_prompt():
+        return
+
+    '''
+    Monitors user response
+    '''
+    def run_response():
+        return
+
+    '''
+    Runs positive bot response
+    '''
+    def run_positive_consequence():
+        return
+
+    '''
+    Runs positive bot response
+    '''
+    def run_negative_consequence():
+        return
+
+    def pause():
+        return
