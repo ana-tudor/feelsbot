3857322f8b7b46933893648e3f03b52ab01ba39a
Ana Tudor <anamtudor@gmail.com>
2018-02-17 23:24:20 -0800

    UserResponse complete and beautiful :3

--- FeelsBotMan/src/UserResponseStage.java
+++ FeelsBotMan/src/UserResponseStage.java
@@ -14,6 +14,8 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
 import com.github.sarxos.webcam.Webcam;
+import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
+import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
@@ -26,20 +28,26 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeC
 
 public class UserResponseStage {
 
-	public static ArrayList<SpeechResults> collectUserSpeechResponse(long time) throws LineUnavailableException, InterruptedException {
+	
+	/**
+	 * Collect the text results of the user's speech
+	 * @param time - hard timeout on collecting response
+	 * @return - list of sentences spoken
+	 */
+	public static ArrayList<SpeechResults> collectUserSpeechResponse(long time)
+			throws LineUnavailableException, InterruptedException {
 		/* Collect audio response and convert to text string */
 		SpeechToText service = new SpeechToText();
-		service.setUsernameAndPassword("45f69b5b-36c5-4e0c-849a-5961ee969f70",
-				"wvBvrJL3q58T");
-		
+		service.setUsernameAndPassword("45f69b5b-36c5-4e0c-849a-5961ee969f70", "wvBvrJL3q58T");
+
 		// Signed PCM AudioFormat with 16kHz, 16 bit sample size, mono
 		int sampleRate = 16000;
 		AudioFormat format = new AudioFormat(sampleRate, 16, 1, true, false);
 		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 
 		if (!AudioSystem.isLineSupported(info)) {
-		  System.out.println("Line not supported");
-		  System.exit(0);
+			System.out.println("Line not supported");
+			System.exit(0);
 		}
 
 		TargetDataLine line = null;
@@ -48,26 +56,22 @@ public class UserResponseStage {
 			line.open(format);
 			line.start();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		AudioInputStream audio = new AudioInputStream(line);
 		ArrayList<SpeechResults> results = new ArrayList();
-		
-		RecognizeOptions options = new RecognizeOptions.Builder()
-		  .interimResults(true)
-		  .inactivityTimeout(2) // use this to stop listening when the speaker pauses, i.e. for 5s
-		  .contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate)
-		  .build();
+
+		RecognizeOptions options = new RecognizeOptions.Builder().interimResults(true).inactivityTimeout(2)
+				.contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate).build();
 
 		service.recognizeUsingWebSocket(audio, options, new BaseRecognizeCallback() {
-			  @Override
-			  public void onTranscription(SpeechResults speechResults) {
+			@Override
+			public void onTranscription(SpeechResults speechResults) {
 				results.add(speechResults);
-//			    System.out.println(speechResults);
-			  }
-			});
+				// System.out.println(speechResults);
+			}
+		});
 
 		try {
 			Thread.sleep(time * 1000);
@@ -75,99 +79,100 @@ public class UserResponseStage {
 			e.printStackTrace();
 		}
 
-		// closing the WebSockets underlying InputStream will close the WebSocket itself.
+		// closing the WebSockets underlying InputStream will close the WebSocket
+		// itself.
 		line.stop();
 		line.close();
-		
-		for(int i = 0; i < results.size(); i++) {
+
+		for (int i = 0; i < results.size(); i++) {
 			if ((i != results.size() - 1) && (results.get(i + 1).getResultIndex() == results.get(i).getResultIndex())) {
 				results.remove(i);
 				results.trimToSize();
 				i--;
 			}
 		}
-		
+
 		return results;
-		
+
 	}
+
 	
-	//Also identify key words!
-	public static double[] evaluateUserSpeechResponse(ArrayList<SpeechResults> results) throws Exception {
+	/**
+	 * Evaluates the speech-based sentiment and keywords from the speakers response
+	 * 
+	 * @param results - results from collecting the user response
+	 * @return average response score, magnitude, and most recent keyword
+	 */
+	public static ArrayList<Object> evaluateUserSpeechResponse(ArrayList<SpeechResults> results) throws Exception {
 		String[] stringResults = new String[results.size()];
-		
-		for(int i = 0; i < results.size(); i++) {
+
+		for (int i = 0; i < results.size(); i++) {
 			stringResults[i] = results.get(i).getResults().get(0).getAlternatives().get(0).getTranscript();
 		}
-		
+
 		double totalScore = 0;
 		double totalMag = 0;
-		
-		for (String text: stringResults) {
-	    	  Sentiment sentiment = parseText(text);
-	    	  System.out.printf("Text: %s%n", text);
-		      System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
-		      
-		      totalScore += sentiment.getScore();
-		      totalMag += sentiment.getMagnitude();
-	    }
-		//modify return
-		double[] ret = {totalScore/((double)stringResults.length), totalMag/((double)stringResults.length)};
+		String keyWord = "";
+
+		for (String text : stringResults) {
+			try (LanguageServiceClient language = LanguageServiceClient.create()) {
+
+				Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+
+				// Detects the sentiment of the text
+				Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+				AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder().setDocument(doc).build();
+				AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+
+				totalScore += sentiment.getScore();
+				totalMag += sentiment.getMagnitude();
+				keyWord = response.getEntitiesList().get(0).getName();
+
+			}
+		}
+		ArrayList<Object> ret = new ArrayList<Object>();
+		ret.add(totalScore / ((double) stringResults.length));
+		ret.add(totalMag / ((double) stringResults.length));
+		ret.add(keyWord);
 		return ret;
 	}
 	
-	//Determine response type
-	
-	public static Sentiment parseText(String text) throws Exception {
-		// Instantiates a client
-		    try (LanguageServiceClient language = LanguageServiceClient.create()) {
-
-		      Document doc = Document.newBuilder()
-		          .setContent(text).setType(Type.PLAIN_TEXT).build();
-
-		      // Detects the sentiment of the text
-		      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
-		      return sentiment;
-	  }
-	}
-	
-	
+	/**
+	 * Collect video data (one image frame) and run analysis on it to return
+	 * sentiment interpretation. --> Capture picture every time this function is
+	 * called --> Save picture as file --> Pass picture filename into vision.py -->
+	 * Get sentiment values --> Delete picture file so we don't use up the
+	 * computer's entire storage --> Return sentiment values (particularly, anger
+	 * and surprise)
+	 */
 	public static void collectUserVisualResponse() {
-		/* Collect video data (one image frame) and run analysis on it to return sentiment interpretation.
-		 * --> Capture picture every time this function is called
-		 * --> Save picture as file
-		 * --> Pass picture filename into vision.py
-		 * --> Get sentiment values
-		 * --> Delete picture file so we don't use up the computer's entire storage
-		 * --> Return sentiment values (particularly, anger and surprise) */
 		
+
 		Webcam webcam = Webcam.getDefault();
 		webcam.open();
-		
+
 		BufferedImage image = webcam.getImage();
-		
+
 		try {
 			ImageIO.write(image, "jpg", new File("test.jpg"));
-		
-		}
-		catch (IOException e) {
-			// idk really what to do so we can just print the shrug emoji and hope this never gets executed
+
+		} catch (IOException e) {
+			// idk really what to do so we can just print the shrug emoji and hope this
+			// never gets executed
 			System.out.println("whoops \\u00AF\\\\_(\\u30C4)_/\\u00AF");
 		}
-		
+
 		try {
 			Process p = Runtime.getRuntime().exec("python vision.py");
 		} catch (Exception e) {
 			System.out.println("oh noes");
 		}
-		
-		
-		
-		
+
 	}
-	
+
 	public static void evaluateUserVisualResponse() {
-		
+
 	}
-	
-	
+
 }
--- FeelsBotMan/src/tests/TextSentiments.java
+++ FeelsBotMan/src/tests/TextSentiments.java
@@ -1,10 +1,13 @@
 package tests;
 
+import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
 import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+
+import java.io.IOException;
 import java.lang.Exception;
 
 public class TextSentiments {
@@ -23,12 +26,44 @@ public class TextSentiments {
 			String[] texts = { "This is a cake", "This is a good cake", "This is a damn good cake",
 					"This is a damned cake", ":)", ":(", "Yes", "No" };
 
-			for (String text : texts) {
-				Sentiment sentiment = parseText(text);
-				System.out.printf("Text: %s%n", text);
-				System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
-			}
+//			for (String text : texts) {
+//				Sentiment sentiment = parseText(text);
+//				AnalyzeEntitiesResponse keywords = parseKeywords(text);
+//				System.out.printf("Text: %s%n", text);
+//				System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
+//				System.out.println(keywords.getEntitiesList());
+//				System.out.println(keywords.getEntitiesList().get(0).getName());
+//			}
 
+			
+//			String[] stringtexts = new String[texts.length];
+//			
+//			for(int i = 0; i < texts.length; i++) {
+//				stringtexts[i] = texts.get(i).gettexts().get(0).getAlternatives().get(0).getTranscript();
+//			}
+			
+			double totalScore = 0;
+			double totalMag = 0;
+			String keyWord = "";
+			
+			for (String text: texts) {
+			      Document doc = Document.newBuilder()
+			          .setContent(text).setType(Type.PLAIN_TEXT).build();
+
+			      // Detects the sentiment of the text
+			      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+			      
+			      AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
+			    		  .setDocument(doc)
+			    		  .build();
+			      AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+			      
+
+			      totalScore += sentiment.getScore();
+			      totalMag += sentiment.getMagnitude();
+			      keyWord = response.getEntitiesList().get(0).getName();
+			      System.out.println(totalScore + " " +  totalMag + " " +  keyWord);
+			}
 		}
 	}
 
@@ -44,10 +79,19 @@ public class TextSentiments {
 		}
 	}
 	
-	public static AnalyzeEntitiesResponse parseKeywords(String text) {
-		
-		
-		return null;
+	public static AnalyzeEntitiesResponse parseKeywords(String text) throws IOException, Exception {
+		try (LanguageServiceClient language = LanguageServiceClient.create()) {
+
+			Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+
+			
+			 AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
+				     .setDocument(doc)
+				     .build();
+			 AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+			 return response;
+				  
+		}
 	}
 	
 }
